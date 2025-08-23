694812de4993beec6ef6042c1e84211abae56e22
Austin Merrick <austinmerrick@onsclom.net>
2025-08-23 09:08:53 -0600

    add animations

--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,12 @@
-import * as UI from "./ui"
+import * as UI from "./ui";
 
 const state = {
   showFPS: UI.ref(false),
   showInternals: UI.ref(false),
   name: UI.ref(""),
   count: 0,
-  frameTimes: [] as number[]
-}
+  frameTimes: [] as number[],
+};
 
 function plural(word: string, count: number) {
   return word + (count === 1 ? "" : "s");
@@ -16,19 +16,26 @@ const fps_measure_window = 1000;
 const fontSize = 16;
 const margin = fontSize / 2;
 const buttonWidth = 100;
-const buttonHeight = 30;
+const buttonHeight = 28;
 const checkboxSize = 20;
 
-export function tick(dt: number, canvasRect: DOMRect, ctx: CanvasRenderingContext2D) {
-  while (state.frameTimes.length > 0 && state.frameTimes[0] <= performance.now() - fps_measure_window) {
+export function tick(
+  _dt: number,
+  canvasRect: DOMRect,
+  ctx: CanvasRenderingContext2D,
+) {
+  while (
+    state.frameTimes.length > 0 &&
+    state.frameTimes[0] <= performance.now() - fps_measure_window
+  ) {
     state.frameTimes.shift();
   }
   const FPS = state.frameTimes.length / (fps_measure_window / 1000);
-  state.frameTimes.push(performance.now())
+  state.frameTimes.push(performance.now());
 
   ctx.textAlign = "left";
-  ctx.textBaseline = "top"
-  ctx.textBaseline = "middle"
+  ctx.textBaseline = "top";
+  ctx.textBaseline = "middle";
   ctx.font = `${fontSize}px Arial`;
   let x = margin;
   let y = margin;
@@ -36,12 +43,23 @@ export function tick(dt: number, canvasRect: DOMRect, ctx: CanvasRenderingContex
   ctx.fillStyle = "#555";
   ctx.fillRect(0, 0, canvasRect.width, canvasRect.height);
 
-  if (UI.button({ text: "+1 count", x, y, height: buttonHeight, width: buttonWidth })) {
+  if (
+    UI.button({
+      text: "+1 count",
+      x,
+      y,
+      height: buttonHeight,
+      width: buttonWidth,
+    })
+  ) {
     state.count++;
   }
   ctx.fillStyle = "white";
-  ctx.fillText(`button clicked ${state.count} ${plural("time", state.count)}`,
-    x + buttonWidth + margin, y + buttonHeight / 2);
+  ctx.fillText(
+    `button clicked ${state.count} ${plural("time", state.count)}`,
+    x + buttonWidth + margin,
+    y + buttonHeight / 2,
+  );
   y += buttonHeight + margin;
 
   UI.checkbox({
@@ -52,17 +70,25 @@ export function tick(dt: number, canvasRect: DOMRect, ctx: CanvasRenderingContex
     height: checkboxSize,
     value: state.showInternals,
   });
-  ctx.fillText(`show internal state`, x + checkboxSize + margin, y + checkboxSize / 2);
+  ctx.fillText(
+    `show internal state`,
+    x + checkboxSize + margin,
+    y + checkboxSize / 2,
+  );
   y += checkboxSize + margin;
 
   if (state.showInternals.value) {
-    const uiState = JSON.stringify({
-      hovering: UI.state.hovering,
-      mouseX: UI.state.mouseX,
-      mouseY: UI.state.mouseY,
-    }, null, 2);
+    const uiState = JSON.stringify(
+      {
+        hovering: UI.state.hovering,
+        mouseX: UI.state.mouseX,
+        mouseY: UI.state.mouseY,
+      },
+      null,
+      2,
+    );
     const uiStateLines = uiState.split("\n");
-    ctx.save()
+    ctx.save();
     ctx.font = `${fontSize}px monospace`;
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
@@ -71,7 +97,7 @@ export function tick(dt: number, canvasRect: DOMRect, ctx: CanvasRenderingContex
       ctx.fillText(line, x, y);
       y += fontSize * 1.2;
     }
-    ctx.restore()
+    ctx.restore();
     y += margin;
   }
 
@@ -108,7 +134,11 @@ export function tick(dt: number, canvasRect: DOMRect, ctx: CanvasRenderingContex
   ctx.textAlign = "left";
   ctx.textBaseline = "middle";
   if (state.name.value !== "") {
-    ctx.fillText(`Hello, ${state.name.value}!`, x + textboxWidth + margin, y + textboxHeight / 2);
+    ctx.fillText(
+      `Hello, ${state.name.value}!`,
+      x + textboxWidth + margin,
+      y + textboxHeight / 2,
+    );
   }
   y += textboxHeight + margin;
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,30 +1,29 @@
-import { canvas } from "./dom"
-import { assert } from "./assert"
+import { canvas } from "./dom";
+import { assert } from "./assert";
 
-const borderColor = "#111"
-const hoveredColor = "#555"
-const nonHoveredColor = "#444"
-const borderSize = 1
-const textColor = "#eee"
+const borderColor = "#111";
+const uiBackgroundColor = "#444";
+const borderSize = 1;
+const textColor = "#eee";
 
-type Ref<T> = { value: T }
+type Ref<T> = { value: T };
 
 export function ref<T>(value: T): Ref<T> {
   return { value };
 }
 
 type UI = {
-  type: "button" | "checkbox" | "textbox",
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  id: string,
-  text?: string, // only for button
-  checkedValue?: Ref<boolean>, // only for checkbox
-  textValue?: Ref<string>, // only for textbox
-  placeholder?: string, // only for textbox
-}
+  type: "button" | "checkbox" | "textbox";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  id: string;
+  text?: string; // only for button
+  checkedValue?: Ref<boolean>; // only for checkbox
+  textValue?: Ref<string>; // only for textbox
+  placeholder?: string; // only for textbox
+};
 
 export const state = {
   ctx: null as CanvasRenderingContext2D | null,
@@ -34,145 +33,192 @@ export const state = {
   mouseX: 0,
   mouseY: 0,
   mouseJustClicked: false,
-  uiToRender: [] as UI[],
+  uiToTick: [] as UI[],
   keysPressed: [] as string[],
-  lastAction: {} as Record<string, number>
-}
+  lastAction: {} as Record<string, number>,
+  animatedHover: {} as Record<string, number>, // approaches 0 (not hovered) or 1 (hovered)
+};
 
 canvas.addEventListener("pointermove", (event) => {
   state.mouseX = event.offsetX;
   state.mouseY = event.offsetY;
-})
+});
 
 canvas.addEventListener("pointerdown", () => {
   state.mouseJustClicked = true;
-})
+});
 
 document.body.addEventListener("keydown", (event) => {
-  // ignore hotkey combinations
   if (event.metaKey || event.ctrlKey || event.altKey) return;
-
   if (state.focused) {
     state.keysPressed.push(event.key);
     event.preventDefault();
   }
-})
+});
 
 export function startTick(ctx: CanvasRenderingContext2D) {
   state.ctx = ctx;
 }
 
+function drawActionFlash(
+  ctx: CanvasRenderingContext2D,
+  actionFactor: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+) {
+  ctx.save();
+  ctx.fillStyle = "white";
+  ctx.globalAlpha = actionFactor * 0.25;
+  ctx.fillRect(x, y, width, height);
+  ctx.restore();
+}
+
 export function endTick(dt: number) {
   // calculate new hovered
   state.hovering = null;
-  for (const ui of state.uiToRender) {
+  for (const ui of state.uiToTick) {
     const { x, y, width, height, id } = ui;
-    const isHovered = state.mouseX >= x && state.mouseX <= x + width &&
-      state.mouseY >= y && state.mouseY <= y + height;
+    const isHovered =
+      state.mouseX >= x &&
+      state.mouseX <= x + width &&
+      state.mouseY >= y &&
+      state.mouseY <= y + height;
     if (isHovered) {
       state.hovering = id;
     }
   }
+
   canvas.style.cursor = state.hovering ? "pointer" : "default";
 
   // calculate clicked
   state.clicked = null;
   if (state.mouseJustClicked) {
     state.clicked = state.hovering;
-    if (state.clicked) { state.lastAction[state.clicked] = performance.now() }
-    // Update focus for textboxes
-    const clickedUI = state.uiToRender.find(ui => ui.id === state.hovering);
-
-    // TODO: make buttons and checkboxes focusable?
-    if (clickedUI?.type === "textbox") {
-      state.focused = state.hovering;
-    } else {
-      state.focused = null;
+    if (state.clicked) {
+      state.lastAction[state.clicked] = performance.now();
     }
+    state.focused = state.hovering;
   }
   state.mouseJustClicked = false;
-
-
-
-  function drawActionFlash(
-    ctx: CanvasRenderingContext2D,
-    actionFactor: number,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-  ) {
-    // action flash?
-    ctx.save()
-    ctx.fillStyle = "white"
-    ctx.globalAlpha = actionFactor * .5
-    ctx.fillRect(x, y, width, height)
-    ctx.restore()
-
-  }
 
   // draw UI & handle updates
   const ctx = state.ctx;
   assert(ctx !== null);
-  for (const ui of state.uiToRender) {
+  for (const ui of state.uiToTick) {
     const actionAnimationTime = 500;
-    const idsLastAction = state.lastAction[ui.id]
-    const timeSinceAction = idsLastAction ?
-      performance.now() - idsLastAction : actionAnimationTime
-    const actionFactor = (1 - (Math.min(timeSinceAction, actionAnimationTime) / actionAnimationTime)) ** 2
+    const idsLastAction = state.lastAction[ui.id];
+    const timeSinceAction = idsLastAction
+      ? performance.now() - idsLastAction
+      : actionAnimationTime;
+    const actionFactor =
+      (1 -
+        Math.min(timeSinceAction, actionAnimationTime) / actionAnimationTime) **
+      2;
+
+    // lets handle hover now
+    const prevAnimatedHover = state.animatedHover[ui.id] ?? 0;
+    const hoverTarget = state.hovering === ui.id ? 1 : 0;
+
+    function lerp(a: number, b: number, percent: number) {
+      const diff = b - a;
+      return a + diff * percent;
+    }
+
+    const newAnimatedHover = lerp(
+      prevAnimatedHover,
+      hoverTarget,
+      dt ** 2 / 1000,
+    );
+    state.animatedHover[ui.id] = newAnimatedHover;
 
     switch (ui.type) {
       case "checkbox": {
         const { x, y, width, height, id, checkedValue: value } = ui;
-        assert(value !== undefined)
-        if (state.clicked === id) { value.value = !value.value; }
+        assert(value !== undefined);
+        if (state.clicked === id) {
+          value.value = !value.value;
+        }
 
         ctx.fillStyle = borderColor;
         ctx.fillRect(x, y, width, height);
-        ctx.fillStyle = state.hovering === id ? hoveredColor : nonHoveredColor;
+        ctx.fillStyle = uiBackgroundColor;
         ctx.fillRect(
           x + borderSize,
           y + borderSize,
           width - borderSize * 2,
-          height - borderSize * 2
-        )
+          height - borderSize * 2,
+        );
+
+        {
+          ctx.save();
+          ctx.fillStyle = "white";
+          ctx.globalAlpha = newAnimatedHover * 0.1;
+          ctx.fillRect(
+            x + borderSize,
+            y + borderSize,
+            width - borderSize * 2,
+            height - borderSize * 2,
+          );
+          ctx.restore();
+        }
+
         if (value.value) {
-          ctx.fillStyle = textColor
+          ctx.fillStyle = textColor;
           ctx.fillRect(x + 5, y + 5, width - 10, height - 10);
         }
-        drawActionFlash(ctx, actionFactor, x, y, width, height)
+        drawActionFlash(ctx, actionFactor, x, y, width, height);
         break;
       }
 
       case "button": {
-        const { x, y, width, height, text, id } = ui;
+        const { x, y, width, height, text } = ui;
         ctx.fillStyle = borderColor;
         ctx.fillRect(x, y, width, height);
 
-        ctx.fillStyle = state.hovering === id ? hoveredColor : nonHoveredColor;
+        ctx.fillStyle = uiBackgroundColor;
         ctx.fillRect(
           x + borderSize,
           y + borderSize,
           width - borderSize * 2,
-          height - borderSize * 2
-        )
+          height - borderSize * 2,
+        );
+
+        {
+          ctx.save();
+          ctx.globalAlpha = newAnimatedHover * 0.1;
+          ctx.fillStyle = "white";
+          ctx.fillRect(
+            x + borderSize,
+            y + borderSize,
+            width - borderSize * 2,
+            height - borderSize * 2,
+          );
+          ctx.restore();
+        }
 
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.fillStyle = textColor;
 
         {
-          ctx.save()
+          ctx.save();
           ctx.beginPath();
-          ctx.rect(x + borderSize, y + borderSize, width - borderSize * 2, height - borderSize * 2);
+          ctx.rect(
+            x + borderSize,
+            y + borderSize,
+            width - borderSize * 2,
+            height - borderSize * 2,
+          );
           ctx.clip();
           ctx.fillText(text || "", x + width / 2, y + height / 2);
           ctx.restore();
         }
 
-        drawActionFlash(ctx, actionFactor, x, y, width, height)
+        drawActionFlash(ctx, actionFactor, x, y, width, height);
 
-        break
+        break;
       }
       case "textbox": {
         const { x, y, width, height, id, textValue, placeholder } = ui;
@@ -197,13 +243,13 @@ export function endTick(dt: number) {
         ctx.fillStyle = borderColor;
         ctx.fillRect(x, y, width, height);
 
-        ctx.fillStyle = nonHoveredColor
+        ctx.fillStyle = uiBackgroundColor;
         ctx.fillRect(
           x + borderSize,
           y + borderSize,
           width - borderSize * 2,
-          height - borderSize * 2
-        )
+          height - borderSize * 2,
+        );
 
         // Render text
         ctx.textAlign = "left";
@@ -220,13 +266,13 @@ export function endTick(dt: number) {
         ctx.rect(x + 4, y, width - 8, height);
         ctx.clip();
 
-        ctx.save()
+        ctx.save();
         ctx.fillStyle = textColor;
         if (!textValue.value && placeholder) {
-          ctx.globalAlpha = 0.5
+          ctx.globalAlpha = 0.5;
         }
         ctx.fillText(displayText, textX, textY);
-        ctx.restore()
+        ctx.restore();
 
         // Draw cursor if focused
         if (isFocused) {
@@ -236,27 +282,26 @@ export function endTick(dt: number) {
         }
         ctx.restore();
 
-        drawActionFlash(ctx, actionFactor, x, y, width, height)
+        drawActionFlash(ctx, actionFactor, x, y, width, height);
         break;
       }
     }
   }
 
-  // Clear processed keys
   state.keysPressed = [];
-  state.uiToRender = [];
+  state.uiToTick = [];
 }
 
 export function checkbox(props: {
-  id: string,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  value: Ref<boolean>,
+  id: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  value: Ref<boolean>;
 }) {
   const { x, y, width, height, value, id } = props;
-  state.uiToRender.push({
+  state.uiToTick.push({
     type: "checkbox",
     x,
     y,
@@ -268,16 +313,16 @@ export function checkbox(props: {
 }
 
 export function button(props: {
-  text: string,
-  x?: number,
-  y?: number,
-  width?: number,
-  height?: number,
-  id?: string,
+  text: string;
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
+  id?: string;
 }): boolean {
   const { text, x = 0, y = 0, width = 100, height = 50 } = props;
   const id = props.id ?? `button-${text}`;
-  state.uiToRender.push({
+  state.uiToTick.push({
     type: "button",
     x,
     y,
@@ -286,20 +331,20 @@ export function button(props: {
     text,
     id,
   });
-  return state.clicked === id
+  return state.clicked === id;
 }
 
 export function textbox(props: {
-  id: string,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  value: Ref<string>,
-  placeholder?: string,
+  id: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  value: Ref<string>;
+  placeholder?: string;
 }): boolean {
   const { x, y, width, height, value, id, placeholder } = props;
-  state.uiToRender.push({
+  state.uiToTick.push({
     type: "textbox",
     x,
     y,
